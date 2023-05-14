039a24227220ed2e7290fe6ecca270dee3776f43
MuhammadN0 <117872290+MuhammadN0@users.noreply.github.com>
2023-05-14 22:09:32 +0300

    zz

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ let sec = document.getElementById("skills");
 let skills = document.querySelectorAll(".skills .skill span");
 console.log(sec)
 console.log(skills)
-window.onscroll = function(){
+// skills.forEach((skill) => {
+//   skill.style.display = 'none'
+// })
+window.addEventListener('scroll', ()=>{
   if (window.scrollY >= sec.offsetTop - 500){
-    skills.forEach(function(e){
-      e.style.width = 100%
+    skills.forEach((e) => {
+      e.style.width = `${e.dataset.width}%`
     })
   }
-}
+})
